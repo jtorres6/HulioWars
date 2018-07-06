f4c4d9a0a8be52667ffc9a8c2af7bc79edd6652d
Salva Corts <salvacortsit@gmail.com>
2018-07-06 21:46:30 +0200

    Pathfinding is working

--- scripts/NPC.gd
+++ scripts/NPC.gd
@@ -4,23 +4,28 @@ var player
 
 
 var life = 100
-var MAX_SPEED = 100
+var MAX_SPEED = 350
 
 var heColisionado
 var alerted = false
 var currentEnemy
+var nav 
 
 
 func _ready():
 	set_process(true)
 	heColisionado = false
+	nav = get_parent().get_node("Navigation2D")
 
 func _process(delta):
-	player = get_parent().get_node("Player")
 	
 	# Si el jugador esta en el radio de alerta del npc, ir a por el
 	if alerted:
-		print("Alertttttt")
+		var enemy = get_parent().get_node("Player")
+		var path = nav.get_simple_path(self.position, enemy.position, false)
+		var dist = self.position.distance_to(path[1])
+		
+		self.set_position(self.position.linear_interpolate(path[1], (MAX_SPEED*delta)/dist))
 	
 
 func _on_Area2D_body_entered(body):
