3d8e51ae62907efe963e4fa5ba21e38f70a64151
Salva Corts <salvacortsit@gmail.com>
2018-07-07 14:45:09 +0200

    Smoother movement in pathfinding and comebak when scape

--- scripts/NPC.gd
+++ scripts/NPC.gd
@@ -5,54 +5,79 @@ const DAMAGE = 20
 
 var life
 var alerted
+var comeback
 var currentEnemy
 var onHitDelay
 var nav 
 var rol
+var originalPosition
 
 func _ready():
 	set_process(true)
 	nav = get_parent().get_node("Navigation2D")
-	#rol = randi()%2 # TODO: MAke it balanced more enemyes tan colleges
-	rol = 1
+	rol = randi()%2 # TODO: Make it balanced more enemyes tan colleges
 	onHitDelay = false
 	alerted = false
+	comeback = false
 	life = 100
+	
+	originalPosition = self.position
+	
+	if rol == 0:
+		self.get_node("Visuals/Sprite").texture = load("res://sprites/npc/police/police1.png")
+	else:
+		self.get_node("Visuals/Sprite").texture = load("res://sprites/npc/punk/punk1.png")
+
 
 func _process(delta):
+	var goal
 	
 	# Si el jugador esta en el radio de alerta del npc, ir a por el
 	if alerted:
-		print(currentEnemy.position)
-		var path = nav.get_simple_path(self.position, currentEnemy.position, false)
+		goal = currentEnemy.position
+	elif comeback:		
+		goal = originalPosition
+
+	if alerted or comeback:
+		var path = nav.get_simple_path(self.position, goal, false)
 		
 		if path.size() > 0:
-			var dist = self.position.distance_to(path[1])
-			self.set_position(self.position.linear_interpolate(path[1], (MAX_SPEED*delta)/dist))
-	
+			for i in range(1, path.size()):
+				var dist = self.position.distance_to(path[i])
+				self.set_position(self.position.linear_interpolate(path[i], (MAX_SPEED*delta)/dist))
+				
+				if comeback and dist < 0.01:
+					comeback = false
 
 func _on_Timer_timeout():
 	onHitDelay = false
 
 
 func _on_Colision_body_entered(body):	
-	if body.name == "Player" and !onHitDelay and self.rol != body.rol: # == Player is unusefull
+	if body.name.find("Projectile") != -1:
+		self.life -= body.damage
+		body.queue_free()
+		
+		if self.life <= 0:
+			self.queue_free()
+	
+	elif body.name == "Player" and !onHitDelay and self.rol != body.rol: # == Player is unusefull
 		body.life -= DAMAGE
 		onHitDelay = true
 		get_node("Timer").start()
-	elif body.name.find("Projectile") != -1:
-		self.life -= body.damage
-		body.queue_free()
 
 
 func _on_ScapeArea_body_exited(enemy):
 	print("Bye ", enemy.name)
 	if enemy == currentEnemy:
+		currentEnemy = null
 		alerted = false
+		comeback = true
 
 
 func _on_AlertArea_body_entered(enemy):
 	print("Hello ", enemy.name)
 	if enemy.rol != self.rol:
-		alerted = true
 		currentEnemy = enemy
+		alerted = true
+		comeback = false
